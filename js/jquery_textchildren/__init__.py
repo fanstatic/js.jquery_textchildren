c905694624ad963e93b4ca35d446866194cad542
Jan-Jaap Driessen <devnull@localhost>
2011-01-20 13:42:14 +0100

    fix name

--- js/jquery_textchildren/__init__.py
+++ js/jquery_textchildren/__init__.py
@@ -1,7 +1,7 @@
 from fanstatic import Library, Resource
 import js.jquery
 
-library = Library('jquery_text_children', 'resources')
+library = Library('jquery_textchildren', 'resources')
 
 textchildren = Resource(library, 'jquery.textchildren.js',
     depends=[js.jquery.jquery])
